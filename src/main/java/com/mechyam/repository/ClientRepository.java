50f5f5430237507a3f9cdd9c159ab3a70de1815c
Mslliksrjun <mallikarjunckm@example.com>
2025-12-29 16:45:09 +0530

    removed the files

--- src/main/java/com/mechyam/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mechyam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mechyam.entity.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
-
